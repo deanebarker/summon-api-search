1a09c4515d7a20c5fb7df08d107c554894e65fd1
Deane Barker <Deane Barker>
2016-11-18 14:29:30 -0600

    Static class for named parameters.

---
+++ Parameter.cs
@@ -0,0 +1,32 @@
+﻿namespace Summon.Core
+{
+    public static class Parameter
+    {
+        public static string Bookmark = "bookMark";
+        public static string Command = "cmd";
+        public static string Debug = "debug";
+        public static string DidYouMean = "dym";
+        public static string QueryExpansion = "exp";
+        public static string FetchMultipleIds = "fids";
+        public static string FacetField = "ff";
+        public static string FacetValueFilter = "fvf";
+        public static string FacetValueGroupFilter = "fvgf";
+        public static string HighlightEnd = "he";
+        public static string HighlightStart = "hs";
+        public static string Highlight = "hl";
+        public static string HoldingsOnly = "ho";
+        public static string Language = "l";
+        public static string LightWeightResponse = "light";
+        public static string MaxRecommendations = "mr";
+        public static string PageNumber = "pn";
+        public static string PageSize = "ps";
+        public static string MaximumTopics = "rec.topic.max";
+        public static string Role = "role";
+        public static string RangeFacetField = "rff";
+        public static string RangeFilter = "rf";
+        public static string ContentRollup = "ru";
+        public static string Sort = "sort";
+        public static string TextFilter = "fq";
+        public static string TextQuery = "q";
+    }
+}
