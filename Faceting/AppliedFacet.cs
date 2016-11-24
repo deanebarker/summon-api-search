1bc8dc899dc58798be49123cad92a110c1dc0918
Deane Barker <deane@blendinteractive.com>
2016-11-24 08:37:59 -0600

    Adjusting namespacing

--- Faceting/AppliedFacet.cs
+++ Faceting/AppliedFacet.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Summon.Core
+namespace Summon.Core.Faceting
 {
     public class AppliedFacet
     {
--- SummonQuery.cs
+++ SummonQuery.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Summon.Core.Faceting;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
